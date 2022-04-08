149274e39277a35e6d5d7045db58aad58325ef05
Andres <andresr774@gmail.com>
2022-04-08 15:31:12 -0500

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Andres camilo Raigoza misas on 8/04/22.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
